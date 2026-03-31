466286900f6a7e4cc1b0edc219635ad6284965f4
riley <riley@poweredbyb.com>
2026-03-31 09:30:24 -0230

    Update GTM container ID to GTM-WVK726MP

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,6 +74,15 @@ export default function RootLayout({
   return (
     <html lang="en-CA">
       <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WVK726MP');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <JsonLd />
       </head>
       <body
@@ -82,7 +91,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TLHHC4G"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WVK726MP"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
@@ -103,15 +112,6 @@ export default function RootLayout({
         <Footer />
         <MobileCallBar />
         <Toaster position="top-center" richColors />
-        {/* Google Tag Manager */}
-        <Script
-          id="gtm"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TLHHC4G');`,
-          }}
-        />
-        {/* End Google Tag Manager */}
       </body>
     </html>
   );
